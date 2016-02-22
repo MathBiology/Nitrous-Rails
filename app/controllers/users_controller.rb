6ce60c08b144586a8a03fa9f747519299401ba1f
Jonathan <jmartin2@ualberta.ca>
2016-02-22 13:30:32 +0000

    Add in sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+        session[:user_id] = @user.id
         flash[:success] = "Welcome to the alpha blog #{@user.username}"
-        redirect_to articles_path
+        redirect_to user_path(@user)
       else
         render 'new'
     end
@@ -44,11 +45,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username,:email,:password)
   end
-  
+
   def set_user
      @user = User.find(params[:id])
   end
-  
+
   def require_same_user
     if current_user != @user
       flash[:danger]= "You can only edit your own account"
